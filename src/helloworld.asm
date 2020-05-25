5a5976b01a387db5a2b10f9a9d03fc0d72818e1a
Simon Murphy <simonmurphy@Simons-MacBook-Pro.local>
2020-05-25 09:53:57 +0100

    Hello world test

--- src/helloworld.asm
+++ src/helloworld.asm
@@ -1,17 +1,24 @@
 ; hello-world.z80
-CHAN_OPEN       equ 5633
-PRINT           equ 8252
+                DEVICE ZXSPECTRUM48
 
-                org 32512
+CHAN_OPEN       equ     5633
+PRINT           equ     8252
 
-                ld      a,2
+                org     $8000
+
+START           ld      a,2
                 call    CHAN_OPEN
                 ld      de, text
                 ld      bc, textend-text
                 jp      PRINT
+                ret
 
 text            defb    'Hello, World!'
 
                 defb    13
 
 textend         equ     $
+
+                ;SAVENEX OPEN "bin/Helloworld.nex", START
+                savesna "bin/Helloworld.sna",START
+                
